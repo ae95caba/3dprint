de4753b79e41a94dbdc77ec5a359a40cd6c889b4
ae95caba <ae95caba@gmail.com>
2024-03-04 08:39:22 -0300

    add the  logic to add the gatsbyImageData on every imageNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -97,23 +97,67 @@ exports.onCreateNode = async ({
   getCache,
   createNodeId,
 }) => {
+  let gatsbyImageDataNode
   if (node.internal.type === "Product") {
     const { createNode } = actions
 
-    /* Download the image and create the File node. Using gatsby-plugin-sharp and gatsby-transformer-sharp the node will become an ImageSharp. */
-
     for (const image of node.images) {
-      await createRemoteFileNode({
-        url: image.url,
-        parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
-        /*    store, // Gatsby's redux store */
-        getCache, // get Gatsby's cache
-        createNode, // helper function in gatsby-node to generate the node
-        createNodeId, // helper function in gatsby-node to generate the node id
+      // Create your node object ////////////////////////////////////////////////
+      const productImageNode = {
+        // Required fields
+        id: `${image.id}`,
+        parent: node.id,
+        internal: {
+          type: `ProductImage`, // name of the graphQL query
+          // contentDigest will be added just after
+          // but it is required
+        },
+        children: [],
 
-        store,
+        // Other fields that you want to query with GraphQL
+        url: image.url,
+
+        // etc...
+      }
+
+      // Get content digest of node. (Required field)
+      const contentDigest = crypto
+        .createHash(`md5`)
+        .update(JSON.stringify(image))
+        .digest(`hex`)
+      // Add it to userNode
+      productImageNode.internal.contentDigest = contentDigest
+
+      // Create node with the Gatsby createNode() API
+      createNode(productImageNode)
+
+      console.log(`productImageNode for ${node.name} created !`)
+
+      ////////////////////////////////////////////////////////////////////////
+    }
+  }
+  if (node.internal.type === "ProductImage") {
+    const { createNodeField, createNode } = actions
+
+    /* Download the image and create the File node. Using gatsby-plugin-sharp and gatsby-transformer-sharp the node will become an ImageSharp. */
+    gatsbyImageDataNode = await createRemoteFileNode({
+      url: node.url,
+      parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
+      /*    store, // Gatsby's redux store */
+      getCache, // get Gatsby's cache
+      createNode, // helper function in gatsby-node to generate the node
+      createNodeId, // helper function in gatsby-node to generate the node id
+
+      store,
+    })
+    console.log(`gastby image data for ${node.name} created !`)
+    ////////////////////////////////////////////////////////////////////////
+    if (gatsbyImageDataNode) {
+      createNodeField({
+        node,
+        name: "gatsbyImageData",
+        value: gatsbyImageDataNode.id,
       })
-      console.log(`image created !`)
     }
   }
 }
